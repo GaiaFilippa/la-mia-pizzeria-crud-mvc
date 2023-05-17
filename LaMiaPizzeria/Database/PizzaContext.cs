f3d33dbc5d8ccade422f973da804521c842fe7c6
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-05-17 15:35:13 +0200

    migration database

---
+++ LaMiaPizzeria/Database/PizzaContext.cs
@@ -0,0 +1,15 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeria.Database
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizze { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzaDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
--- LaMiaPizzeria/Models/Pizza.cs
+++ LaMiaPizzeria/Models/Pizza.cs
@@ -1,6 +1,31 @@
-﻿namespace LaMiaPizzeria.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaMiaPizzeria.Models
 {
     public class Pizza
     {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [MaxLength(300)]
+        public string Image { get; set; }
+
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
     }
 }
