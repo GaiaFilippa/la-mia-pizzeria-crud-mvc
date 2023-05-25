35fda0ff502452483e202f5d0863ec808482751e
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-05-25 14:01:19 +0200

    Added class PizzaCategory

---
+++ LaMiaPizzeria/Models/PizzaCategory.cs
@@ -0,0 +1,23 @@
+﻿namespace LaMiaPizzeria.Models
+{
+	public class PizzaCategory
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string? Description { get; set; }
+
+		// Ricorda la lista
+
+		public PizzaCategory()
+		{
+
+		}
+
+		public PizzaCategory(string name, string? description)
+		{
+			Name = name;
+			Description = description;
+		}
+
+	}
+}
