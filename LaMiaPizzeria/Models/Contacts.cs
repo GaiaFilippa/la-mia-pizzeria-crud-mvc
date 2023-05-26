b1a93d4bfc27e7d078c00aff8482a5378983d1c8
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-05-26 14:57:22 +0200

    modified class Contacts

---
+++ LaMiaPizzeria/Models/Contacts.cs
@@ -0,0 +1,24 @@
+﻿namespace LaMiaPizzeria.Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Feedback { get; set; }
+
+        public Contacts()
+        {
+           
+        }
+
+        public Contacts(string name, string surname, string email, string feedback)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Feedback = feedback;
+        }
+    }
+}
